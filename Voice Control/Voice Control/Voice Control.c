77f9b70f084af16e1d8aa1c29187ad16d2e820c4
cwilliams88 <cwilliams88@19b3486b-eadb-e237-ab27-9de013702325>
2012-02-15 18:15:42 +0000

    Adding case statements and such.

--- Voice Control/Voice Control/Voice Control.c
+++ Voice Control/Voice Control/Voice Control.c
@@ -7,14 +7,68 @@
 
 #include "capi324v221.h"
 
+/* FUNCTION PROTOTYPES */
+void goForward();
+void goBackward();
+void turnRight();
+void turnLeft();
+
 void CBOT_main( void )
-{
+{	
+	uint8_t cmd = 0;             // Store a single byte
+	uint8_t state = 0;
+	uint8_t prev_state = 0;
+	
+	/* Setting Up */
 	// Open and initialize the LCD-subsystem.
 	LCD_open();
+	STEPPER_open();     // Open STEPPER module for use.
+	UART_open(UART_UART0);
+	UART_configure(UART_UART0, UART_8DBITS, UART_1SBIT, UART_NO_PARITY, 9600);
+	
 	// Clear the LCD.
 	LCD_clear();
 	// Print a message.
 	LCD_printf( "Hello, Dolly!\n" );
 	// Don't leave.
-	while( 1 );
+	
+	while( 1 )
+	{
+		prev_state = state;
+		state = cmd;
+		switch (state) {
+        case 5: /* execute unit of code 1 */
+			
+            break;
+        case 10: /* Command to turn right */
+		 
+            break;
+        default: /* execute default action */
+            break;
+    }
+		
+	}
 } // end CBOT_main()
+
+void goForward()
+{
+	STEPPER_run( STEPPER_BOTH, STEPPER_FWD, 150 );
+}
+void goBackward()
+{
+	STEPPER_run( STEPPER_BOTH, STEPPER_REV, 150 );
+}
+void turnLeft()
+{
+	//TURN LEFT (~90-degrees)...
+	STEPPER_move_stwt( STEPPER_BOTH,
+		STEPPER_REV, 150, 200, 400, STEPPER_BRK_OFF,   // Left
+		STEPPER_FWD, 150, 200, 400, STEPPER_BRK_OFF ); // Right
+}
+void turnRight()
+{
+	//TURN RIGHT (~90-degrees)...
+	STEPPER_move_stwt( STEPPER_BOTH,
+		STEPPER_FWD, 150, 200, 400, STEPPER_BRK_OFF,   // Left
+		STEPPER_REV, 150, 200, 400, STEPPER_BRK_OFF ); // Right
+}
